3e12e0b2758a4b580b1ae30c389c2ed2ca1c8c1d
Darshan Vedpathak <dvedpathak007@gmail.com>
2022-03-12 13:34:50 +0530

    Jyupiter notebook odd even

--- Average_marks.py
+++ Average_marks.py
@@ -1,10 +1,8 @@
 # Read input as sepcified in the question
 # Print output as specified in the question
-M1=int(input())
-M2=int(input())
-M3=int(input())
-
+M1,M2,M3 = int(input()), int(input()),int(input())
 sum=M1+M2+M3
 
 avg=sum/3
 print(avg)
+
